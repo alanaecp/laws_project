37220065d0ee0ea0d6f9ee60a8967493b45ed8fd
Alana Oliveira <alana@ecp.ufma.br>
2021-02-14 16:59:24 -0300

    do deploy

--- laws/settings.py
+++ laws/settings.py
@@ -128,3 +128,10 @@ STATICFILES_DIRS = [
 # Configurar a pasta de Media (Uploads)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
